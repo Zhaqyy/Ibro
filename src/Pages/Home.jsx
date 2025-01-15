e02b49650c5f14f1382882d17e50f2482a73f7f6
zhaqyy <zaqrashyy7@gmail.com>
2025-01-15 02:37:53 +0100

    feat - better blur and filter

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,7 +11,6 @@ function Home() {
   const svgRef = useRef(null);
   const heroRef = useRef(null);
 
-
   useEffect(() => {
     gsap.set(svgRef.current, { xPercent: -50, yPercent: -50 });
 
@@ -22,13 +21,12 @@ function Home() {
     let mouseX = 0;
     let mouseY = 0;
 
-
     const handleMouseMove = e => {
       mouseX = e.clientX;
       mouseY = e.clientY;
 
-        xTo(mouseX);
-        yTo(mouseY);
+      xTo(mouseX);
+      yTo(mouseY);
     };
 
     // Attach mousemove event listener
@@ -41,7 +39,6 @@ function Home() {
 
   return (
     <section className='hero' ref={heroRef}>
-
       <svg
         version='1.1'
         xmlns='http://www.w3.org/2000/svg'
@@ -50,15 +47,17 @@ function Home() {
         height='100%'
         id='masker'
       >
-        <filter id='blur-image' color-interpolation-filters="sRGB">
-          <feGaussianBlur in='SourceGraphic' stdDeviation='15 30'></feGaussianBlur>
+        <filter id='blur-image' color-interpolation-filters='sRGB'>
+          <feGaussianBlur in='SourceGraphic' stdDeviation='15 30' result="blur"></feGaussianBlur>
+          {/* <feTurbulence type='fractalNoise' baseFrequency='0.95' numOctaves='3' stitchTiles='stitch' />
+          <feColorMatrix type='saturate' values='0' result='grain' />
+          <feComposite operator='in' in='blur' in2='grain' /> */}
         </filter>
         <mask id='blur-mask'>
-          <rect x="0" y="0" height="150" width="250" rx="10"fill="#fff"   ref={svgRef} id='mask' />
-
+          <rect x='0' y='0' height='150' width='250' rx='10' fill='#fff' ref={svgRef} id='mask' />
         </mask>
-        <image xlinkHref='./ibro.jpg' width='100%' height='100%' filter='url(#blur-image)' preserveAspectRatio="xMidYMax slice" />
-        <image xlinkHref='./ibro.jpg' width='100%' height='100%' mask='url(#blur-mask)' preserveAspectRatio="xMidYMax slice" />
+        <image xlinkHref='./ibro.jpg' width='100%' height='100%' filter='url(#blur-image)' preserveAspectRatio='xMidYMax slice' />
+        <image xlinkHref='./ibro.jpg' width='100%' height='100%' mask='url(#blur-mask)' preserveAspectRatio='xMidYMax slice' />
       </svg>
       <h1 className='bigName'>IBRAHIM SHUAIB</h1>
       <div className='menu'>
