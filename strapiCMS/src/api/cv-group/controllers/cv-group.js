c5207a6a4f8557ee2e3212ff2178265e3f903315
zhaqyy <zaqrashyy7@gmail.com>
2025-04-17 16:58:04 -0600

    removed strapi

--- strapiCMS/src/api/cv-group/controllers/cv-group.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cv-group controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::cv-group.cv-group');
--- strapiCMS/src/api/cv-group/routes/cv-group.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cv-group router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::cv-group.cv-group');
